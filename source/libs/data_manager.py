71727406990158bc7038ddcda514e8183c7f58ba
Brian Weisemann <brianw7693@gmail.com>
2025-03-11 17:12:42 -0300

    feat(data_manager): implement 'time_filter' method

--- source/libs/data_manager.py
+++ source/libs/data_manager.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 from typing import Optional
 
@@ -12,9 +13,18 @@ from source.types.logger_types import TermLoggerType
 
 @dataclass
 class Config(BaseConfig):
+    default_field_name: Optional[str] = None
     pandas_log_use_custom_settings: bool = True
     pandas_log_display_width: int = 1000
     pandas_log_display_max_cols: Optional[int] = None
+
+
+class UndefinedFieldName(Exception):
+    pass
+
+
+class InvalidTimeRange(Exception):
+    pass
 
 
 class DataManager(BaseClass):
@@ -48,6 +58,51 @@ class DataManager(BaseClass):
         if self._dynamic_verbose_level != VerboseLevel.NONE:
             self._logger.debug(TermLoggerType.SHORT, f'Length: {df_length}')
         return df_length
+
+    @base_method
+    def time_filter(self,
+                    field_name: Optional[str] = None,
+                    time_from: Optional[datetime] = None,
+                    time_to: Optional[datetime] = None,
+                    count_from_start: Optional[int] = None,
+                    count_to_end: Optional[int] = None,
+                    ) -> DataFrame:
+        if field_name is None:
+            if self._config.default_field_name is None:
+                raise UndefinedFieldName(
+                    'Both the "field_name" parameter and the "default_field_name" option were not set. At least one of them is required.')
+            else:
+                field_name = self._config.default_field_name
+
+        time_from_as_str = None
+        time_from_condition = lambda: True
+        if time_from is not None:
+            time_from_as_str = str(time_from)
+            time_from_condition = lambda: (self.__dataframe[field_name] >= time_from_as_str)
+
+        time_to_as_str = None
+        time_to_condition = lambda: True
+        if time_to is not None:
+            time_to_as_str = str(time_to)
+            time_to_condition = lambda: (self.__dataframe[field_name] < time_to_as_str)
+
+        if time_from is not None and time_to is not None and time_from >= time_to:
+            raise InvalidTimeRange(
+                f'"time_from" ({time_from_as_str}) must be earlier than "time_to" ({time_to_as_str}).')
+
+        self.__dataframe = self.__dataframe.loc[time_from_condition() & time_to_condition()]
+
+        if count_from_start is not None:
+            self.__dataframe = self.__dataframe[:count_from_start]
+        if count_to_end is not None:
+            self.__dataframe = self.__dataframe[-count_to_end::]
+
+        if self._dynamic_verbose_level != VerboseLevel.NONE:
+            self._logger.debug(TermLoggerType.SHORT,
+                               f'Data was filtered:\nFrom: {time_from_as_str} (Start Count: {count_from_start}) - To: {time_to_as_str} (End Count: {count_to_end})')
+            self._logger.debug(TermLoggerType.SHORT, f'Sample:\n{self.__dataframe}')
+
+        return self.__dataframe
 
     @base_method
     def destroy(self):
